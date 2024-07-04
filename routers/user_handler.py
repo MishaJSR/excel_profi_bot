6240fdfdf701d0052c2397c064c1bd2cfc8d93ad
Michail S <mixan_45@mail.ru>
2024-07-04 11:35:41 +0300

    1.0.1

--- routers/user_handler.py
+++ routers/user_handler.py
@@ -9,6 +9,9 @@ load_dotenv(find_dotenv())
 
 user_private_router = Router()
 
+@user_private_router.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer('Для начала работы введите артикул')
 
 @user_private_router.message(StateFilter('*'))
 async def start_cmd(message: types.Message):
@@ -16,14 +19,15 @@ async def start_cmd(message: types.Message):
     if df.empty:
         return
     articul = message.text
-    df['Приход'] = df['Приход'].dt.strftime('%d-%m-%Y %H:%M:%S')
+    timestamp_columns = df.select_dtypes(include='datetime').columns
+    df[timestamp_columns[0]] = df[timestamp_columns[0]].dt.strftime('%d-%m-%Y')
     try:
         index = df[df['Артикул'] == int(articul)].index[0]
         row_data = df.loc[index]
         arguments = list(row_data.values)[1:]
-        for el in arguments:
-            await message.answer(el)
-    except IndexError:
+        for index, el in enumerate(arguments):
+            await message.answer(f'{df.columns[index + 1]}: {el}')
+    except:
         await message.answer('По данному артикулу совпадений не найдено')
 
 
@@ -33,7 +37,7 @@ async def start_cmd(message: types.Message):
 async def check_table(message):
     df = pd.DataFrame()
     try:
-        df = pd.read_excel('excel_docs/table.xl')
+        df = pd.read_excel('excel_docs/table.xlsx')
     except FileNotFoundError:
         await message.answer(f'Таблица не найдена\nПроверьте наличие файла table.xlsx в папке excel_docs')
     return df
